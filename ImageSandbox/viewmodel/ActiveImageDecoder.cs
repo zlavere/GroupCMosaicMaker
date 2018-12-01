379f15adce08a5f5d1d92348728096939386a273
Zachary LaVere <zlavere@westga.edu>
2018-12-01 12:54:22 -0500

    changed gitignore

---
+++ ImageSandbox/viewmodel/ActiveImageDecoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+
+namespace ImageSandbox.ViewModel
+{
+    public static class ActiveImageDecoder
+    {
+        public static BitmapDecoder Decoder { get; set; }
+    }
+}
